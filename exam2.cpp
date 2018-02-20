cf02e792a75ea51f281f223c41c164ce225ea0b4
Unknown <toey.prapaporn123@gmail.com>
2018-02-20 13:10:02 +0700

    ข้อ 2

--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
+using namespace std;
 int main() {
+    int cmd;
+    string name;
+    string line;
+    ofstream writefile;
+    ifstream readfile;
 
-        return 0;
-}
+
+    do {
+        cout<<"MENU"<<endl;
+        cout<<"1.Add Student."<<endl;
+        cout<<"2.List Student."<<endl;
+        cout<<"3.Student."<<endl;
+        cout<<"4.Sarch List Student."<<endl;
+        cout<<"5.Quit."<<endl;
+        cout<<"Please Enter Menu"<<endl;
+        cin>>cmd;
+          if (cmd == 1){
+            cout <<"Enter Student Name : ";
+            cin>>name;
+            writefile.open("std.txt",fstream::app);
+            if (writefile.is_open()){
+                writefile<<name<<endl;
+                writefile.close();
+            }else {
+                    cout<<"Unable to open file";
+            }
+        }else if (cmd==2){
+                    readfile.open("std.txt");
+                    if (readfile.is_open()){
+                        while(getline(readfile,line)){
+                            cout<<line<<endl;
+                        }
+                        readfile.close();
+                    }
+        }else if(cmd==3){
+                    cout<<"Enter Name : ";
+                    cin>>name;
+                    readfile.open("std.txt");
+                    if(readfile.is_open()){
+                    (name="&name&",name);
+                      while(getline(readfile,line)){
+                            cout<<name<<endl;
+                        }
+        }
+        }
+        else if (cmd==4){
+                    cout<<"Enter Name : ";
+                    cin>>name;
+                    readfile.open("std.txt");
+                    if(readfile.is_open()){
+                    (name="&name&",name);
+                      while(getline(readfile,line)){
+                            cout<<name<<endl;
+                        }
+        }
+        }else{break;}
+    }while(1);
+       return 0;
+    }
+
